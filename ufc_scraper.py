9b52e3b86c8bf3703c6893c01a28561096804f56
Jacob1802 <jacobrdilonardo@gmail.com>
2023-05-09 17:21:17 +1000

    complete function to scrape UFC fight statistics from website

--- ufc_scraper.py
+++ ufc_scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from typing import List
 from datetime import datetime
+import time
 import re
 
 
@@ -30,7 +31,7 @@ def get_events() -> List[str]:
         href = link['href']
         event_links.append(href)
     
-    return event_links
+    return event_links[::-1]
 
 
 def extract_event_details():
@@ -57,43 +58,119 @@ def extract_event_details():
 
 
 def extract_fight_details():
+    s = time.time()
     data = extract_event_details()
+    rows = []
     for event in data:
-        for fight in event['fight_details']:
+        date = event['date']
+        location = event['location']
+        for i, fight in enumerate(event['fight_details'], start=1):
+            row = {"fight_num" : i, "date": date, "location": location}
             response = requests.get(fight)
             soup = BeautifulSoup(response.content, "html.parser")
+            names = [name.text.strip() for name in soup.find_all("h3", class_="b-fight-details__person-name")]
+            for i, name in enumerate(names, start=1):
+                row[f'fighter_{i}'] = name
+                
+            # Add fight details (method, round, time, format, ref)
+                details = [i for detail in soup.find_all("p", class_="b-fight-details__text") for i in detail.stripped_strings]
+                for i in range(0, len(details), 2):
+                    if details[i] == "Details:":
+                        row[details[i]] = " ".join(details[i+1:])
+
+                        break
+                    else:
+                        row[details[i]] = details[i + 1]
+            stats = soup.find_all("tbody", class_="b-fight-details__table-body")
             
-        return
+            for j in range(len(stats)):
+                data = [i for i in stats[j].stripped_strings]
+
+                if j < 2:
+                    CATEGORIES = [    
+                    ("fighter", [0, 20, 40, 60, 80]),
+                    ("kd", [2, 22, 42, 62, 82]),
+                    ("sig_str", [4, 24, 44, 64, 84]),
+                    ("sig_str_%", [6, 26, 46, 66, 86]),
+                    ("total_str", [8, 28, 48, 68, 88]),
+                    ("td", [10, 30, 50, 70, 90]),
+                    ("td_%", [12, 32, 52, 72, 92]),
+                    ("sub_attempt", [14, 34, 54, 74, 94]),
+                    ("reversal", [16, 36, 56, 76, 96]),
+                    ("ctrl", [18, 38, 58, 78, 98])
+                    ]
+                else:
+                    CATEGORIES = [    
+                    ("fighter", [0, 18, 36, 54, 72]),
+                    ("sig_str", [2, 20, 38, 56, 74]),
+                    ("sig_str%", [4, 22, 40, 58, 76]),
+                    ("head", [6, 24, 42, 60, 78]),
+                    ("body", [8, 26, 44, 62, 80]),
+                    ("leg", [10, 28, 46, 64, 82]),
+                    ("distance", [12, 30, 48, 66, 84]),
+                    ("clinch", [14, 32, 50, 68, 86]),
+                    ("ground", [16, 34, 52, 70, 88]),
+                    ]
+                inc = 0
+                current_round = None
+                for i in range(0, len(data), 2):
+                    for category_name, category_indices in CATEGORIES:
+                        if i in category_indices:
+                            key = category_name
+                            break
+                    try:
+                        if data[i+inc].startswith("Round"):
+                            current_round = data[i+inc].replace(" ", "_").lower()
+                            inc += 1
+
+                    except IndexError:
+                        break
+                    if key != "fighter":
+                        if current_round is not None:
+                            row[current_round + "_" + key + "_1"] = data[i+inc]
+                            row[current_round + "_" + key + "_2"] = data[i+1+inc]
+                        else:
+                            row["total_" + key + "_1"] = data[i+inc]
+                            row["total_" + key + "_2"] = data[i+1+inc]
+            print(row)
+            return
+        
+        rows.append(row)
+        
         
 
 def get_fighter_links() -> List[str]:
+    start = time.time()
+    
     """Extract fighter profile links from ufcstats.com
 
     Returns:
         List[str]: links to ufc fighter profile
     """
+    alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
     fighter_links = set()
-    # url = "http://www.ufcstats.com/statistics/fighters?char=a&page=all"
-    url = "http://www.ufcstats.com/statistics/fighters"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    # Find all links on the page with the class 'b-link b-link_style_black'
-    links = soup.find_all('a', class_='b-link b-link_style_black')
+    for letter in alphabet:
+        url = f"http://www.ufcstats.com/statistics/fighters?char={letter}&page=all"
+        # url = "http://www.ufcstats.com/statistics/fighters"
+        session = requests.Session()
+        response = session.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        # Find all links on the page with the class 'b-link b-link_style_black'
+        links = soup.find_all('a', 'b-link b-link_style_black')
+        # Extract the href attribute from each link and append it to fighter_links
+       
+        for link in links:
+            fighter_links.add(link['href'])
 
-    # Extract the href attribute from each link and append it to fighter_links
-    for link in links:
-        href = link['href']
-        if href not in fighter_links:
-            fighter_links.add(href)
     
-    return fighter_links
+    return list(fighter_links), start
 
 
 def extract_fighter_stats():
-    links = get_fighter_links()
+    links, start = get_fighter_links()
     rows = []
     # if fighter not in fights since last run skip
-    for fighter_link in links[:5]:
+    for fighter_link in links:
         response = requests.get(fighter_link)
         soup = BeautifulSoup(response.content, "html.parser")
         name = soup.find("span", class_="b-content__title-highlight").text.strip()
@@ -131,30 +208,29 @@ def extract_fighter_stats():
                         key = key.lower().replace(" ", "_").replace(".", "")
                         
                 row[key.lower()] = value
-                
-        if 'dob' in row.keys():
-            del row['dob']
-            
+
         rows.append(row)
     
     df = pd.DataFrame(rows)
+    if 'dob' in df.columns:
+        df.drop('dob', axis=1, inplace=True)
+    
+    df = df.sort_values('fighter')
     df.to_csv("fighters.csv", index=False)
+    print(time.time() - start)
 
 
 def inch_to_cm(height) -> float:
     
     if "'" in height:
         feet, inches = height.split("'")
-        feet = int(feet)
-        inches = int(inches.strip('"'))
-        total_inches = (feet * 12) + inches
+        total_inches = (int(feet) * 12) + int(inches.strip('"'))
         cm = total_inches * 2.54
         return round(cm, 1)
     
     elif '"' in height:
         inches = int(height.strip('"'))
-        cm = inches * 2.54
-        return round(cm, 1)
+        return round(inches * 2.54, 1)
 
 
 if __name__ == "__main__":
