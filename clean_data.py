4d3e609098d4333a56d7ff4d8acce28f25271e9d
Jacob1802 <jacobrdilonardo@gmail.com>
2023-05-23 12:49:25 +1000

    update scraping function to have round by round scraping and just total fight stats, implement lag, differential, avg, weighted_avg & totals features. Split fights dataframe function to work with data easily & reformat data function

---
+++ clean_data.py
@@ -0,0 +1,282 @@
+import pandas as pd
+import datetime as dt
+import numpy as np
+import time
+
+
+def main():
+    input_file = pd.read_csv("raw_fight_totals.csv")
+    
+    new = split_dataframe(input_file)
+    df = reformat_data(new)
+    df = add_new_features(df)
+    df.to_csv("fights.csv", index=False)
+
+
+def split_dataframe(df):
+    details_columns = ['fight_num', 'date', 'fighter', 'result', 'weight_class']
+    method_columns = ['method', 'time format', 'time', 'round']
+    cols = ['sig_str_landed', 'sig_str_attempts', 'sig_str_received', 'sig_str_avoided', 'sig_str_percent', 'str_landed', 
+        'str_attempts', 'str_received', 'str_avoided', 'str_percent', 'td_landed', 'td_attempts', 'td_received', 'td_avoided', 
+        'td_percent', 'kd', 'kd_received', 'sub_att', 'rev', 'ctrl', 'sig_reg_mixture', 'sig_reg_percent']
+    
+    new_columns = details_columns + method_columns + cols
+
+    new_data = []
+    
+    for _, row in df.iterrows():
+        date_row = [row['fight_num'], row['date']]
+        result_1_row = [row['result_1']]
+        result_2_row = [row['result_2']]
+        fighter_1_row = [f"{row['fighter_1']}"]
+        fighter_2_row = [f"{row['fighter_2']}"]
+        weightclass_row = [row['weight_class']]
+        method_row = [row[col] for col in method_columns]
+        
+        row_1 = [row[f"{col}_1"] for col in cols]
+        row_2 = [row[f"{col}_2"] for col in cols]
+        
+        new_data.append(date_row + fighter_1_row + result_1_row + weightclass_row + method_row + row_1)
+        new_data.append(date_row + fighter_2_row + result_2_row + weightclass_row + method_row + row_2)
+
+    new_df = pd.DataFrame(new_data, columns=new_columns)
+    return new_df
+
+
+def reformat_data(df):
+    ufc_weight_classes = [
+        "men_strawweight",
+        "men_flyweight",
+        "men_bantamweight",
+        "men_featherweight",
+        "men_lightweight",
+        "men_welterweight",
+        "men_middleweight",
+        "men_light_heavyweight",
+        "men_heavyweight",
+        "womens_strawweight",
+        "womens_flyweight",
+        "womens_bantamweight",
+        "womens_featherweight",
+    ]
+
+    df['method'] = df['method'].replace({
+        "TKO - Doctor's Stoppage": "KO/TKO",
+        "Decision - Unanimous": "Decision",
+        "Decision - Split": "Decision",
+        "Decision - Majority": "Decision",
+        "Overturned": "Other",
+        "DQ": "Other",
+        "Could Not Continue": "Other"
+    })
+
+    df = df.fillna(0)
+    for col in ["sig_str_percent", "td_percent"]:
+        df[col] = df[col].str.strip("%").replace("---", "0").fillna(0).apply(lambda x: int(x) / 100)
+    df['date'] = pd.to_datetime(df['date'])
+    df['weight_class'] = df['weight_class'].str.replace("UFC", "").str.replace("Title", "")
+    df['weight_class'] = df['weight_class'].str.replace("Bout", "").str.strip().str.replace(" ", "_")
+    df['weight_class'] = df['weight_class'].str.replace("Women's", "womens").str.lower()
+    df['weight_class'] = df['weight_class'].apply(lambda x: "men_" + x if not x.startswith("women") else x)
+    df['weight_class'] = df['weight_class'].apply(lambda x: "catchweight" if x.lower() not in ufc_weight_classes else x)
+    # Remove any 'OT' occurrences if present and convert to datetime object
+    df['time'] = pd.to_datetime(df['time'].str.replace('OT', ''), format='%M:%S').apply(convert_to_minutes) # Convert to minutes  
+    
+    df['ctrl'] = pd.to_datetime(df['ctrl'], format='%H:%M:%S', errors='coerce').fillna(pd.to_datetime('00:00:00', format='%H:%M:%S')).apply(convert_to_minutes) # Convert to minutes
+    
+    df = df.rename(columns={'round': 'round_finished'})
+    # df['rounds'] = df['time format'].apply(lambda x: "1" if x == "No Time Limit" else x).str.extract('^(\d+)', expand=False).astype(int)
+    df = df.drop('time format', axis=1)
+    return df
+
+
+def add_new_features(df):
+    totals = ['kd', 'kd_received','round_finished','sig_str_landed','sig_str_received','sig_str_attempts',
+        'sig_str_avoided','str_received','str_attempts', 'str_avoided','str_landed',
+        'td_received','td_attempts','td_avoided','td_landed']
+        
+    damage_cols = ['sig_str_landed',
+       'sig_str_attempts', 'sig_str_received', 'sig_str_avoided',
+       'str_landed', 'str_attempts', 'str_received',
+       'str_avoided']
+
+    differentials = ['sig_str_landed', 'str_landed']
+    
+    lags = damage_cols + ['kd', 'kd_received',
+       'td_landed', 'td_attempts', 'td_received', 'td_avoided',
+       'sub_att', 'rev']
+    
+    accuracy = ['sig_str', 'str', 'td']
+
+    methods = ['Decision','KO/TKO','Other','Submission']
+    
+    grouped_fighters = df.groupby('fighter', group_keys=False)
+    
+    df['num_fights'] = grouped_fighters.cumcount()
+    df['total_fight_time'] = grouped_fighters['time'].cumsum()
+    df['days_since_last_fight'] = grouped_fighters['date'].apply(lambda x: x - x.shift(1))
+    
+    # Total wins, losses & w/l by decison, ko, sub, other
+    for result in ["W", "L"]:
+        df["total_" + result.lower()] = grouped_fighters.apply(lambda x: (x['result'] == result).shift(1).cumsum())
+        for method in methods:
+            df[result.lower() + "_" + method.lower()] = grouped_fighters.apply(lambda x: ((x['result'] == result) & (x['method'] == method)).shift(1).cumsum())
+
+    # for col in accuracy:
+    #     df[col + '_accuracy'] = df.apply(calculate_accuracy(df[col + '_attempts'], df[col + '_landed']))
+    
+    for col in differentials:
+        differential(df, col)
+        
+    for col in lags + ['sig_str_landed_differential', 'str_landed_differential']:
+        for i in range(1, 4):
+            lag(df, col, i)
+        
+        calculate_weighted_avg(df, col)
+        calculate_avg(df, col)
+    
+    # Group the DataFrame by 'fighter' column
+    grouped_df = df.groupby('fighter', group_keys=False)
+
+    for col in totals:
+        if col == "round_finished":
+            df["foo"] = grouped_df[col].cumsum()
+            df['total_rounds1'] = grouped_df['foo'].shift(1)
+        else:
+            df["foo1"] = grouped_df[col].cumsum()
+            df["total_" + col] = grouped_df['foo1'].shift(1)
+            
+    df = df.drop(['foo', 'foo1'], axis=1)
+    
+    # Iterate over each group
+    for _, group in grouped_df:
+        # Reset streak counters for each group
+        current_win_streak = 0
+        current_loss_streak = 0
+        
+        # Iterate over rows in the group
+        for index, row in group.iterrows():
+            # Update streak columns for the current row
+            df.at[index, 'win_streak'] = current_win_streak
+            df.at[index, 'loss_streak'] = current_loss_streak
+            if row['result'] == 'W':
+                # Increment win streak and reset loss streak
+                current_win_streak += 1
+                current_loss_streak = 0
+            elif row['result'] == 'L':
+                # Increment loss streak and reset win streak
+                current_loss_streak += 1
+                current_win_streak = 0
+            else:
+                current_win_streak = 0
+                current_loss_streak = 0
+
+    return df
+
+
+def add_new_features(df):
+    totals = ['kd', 'kd_received', 'round_finished', 'sig_str_landed', 'sig_str_received', 'sig_str_attempts',
+              'sig_str_avoided', 'str_received', 'str_attempts', 'str_avoided', 'str_landed',
+              'td_received', 'td_attempts', 'td_avoided', 'td_landed']
+
+    damage_cols = ['sig_str_landed', 'sig_str_attempts', 'sig_str_received', 'sig_str_avoided',
+                   'str_landed', 'str_attempts', 'str_received', 'str_avoided']
+
+    differentials = ['sig_str_landed', 'str_landed']
+
+    lags = damage_cols + ['kd', 'kd_received', 'td_landed', 'td_attempts', 'td_received', 'td_avoided',
+                          'sub_att', 'rev']
+
+    accuracy = ['sig_str', 'str', 'td']
+
+    methods = ['Decision', 'KO/TKO', 'Other', 'Submission']
+
+    grouped_fighters = df.groupby('fighter', group_keys=False)
+
+    temp_df = pd.DataFrame()
+
+    temp_df['num_fights'] = grouped_fighters.cumcount()
+    temp_df['total_fight_time'] = grouped_fighters['time'].cumsum()
+    temp_df['days_since_last_fight'] = grouped_fighters['date'].diff()
+
+    for result in ["W", "L"]:
+        temp_df["total_" + result.lower()] = grouped_fighters['result'].apply(lambda x: (x == result).shift().cumsum())
+        for method in methods:
+            temp_df[result.lower() + "_" + method.lower()] = grouped_fighters.apply(
+                lambda x: ((x['result'] == result) & (x['method'] == method)).shift().cumsum())
+
+    for col in differentials:
+        temp_df[col + '_diff'] = grouped_fighters[col].diff()
+        
+    for col in lags + ['sig_str_landed_diff', 'str_landed_diff']:
+        for i in range(1, 4):
+            temp_df[col + '_lag' + str(i)] = grouped_fighters[col].shift(i)
+        
+        temp_df[col + '_weighted_avg'] = temp_df.groupby('fighter')[col].transform(lambda x: np.average(x, weights=df['round_finished']))
+        temp_df[col + '_avg'] = temp_df.groupby('fighter')[col].transform('mean')
+
+    temp_df['total_rounds'] = grouped_fighters['round_finished'].cumsum().shift()
+    temp_df['total_rounds'] = temp_df.groupby('fighter')['total_rounds'].transform('first')
+
+    win_streak = (df['result'] == 'W').groupby(df['fighter']).cumsum()
+    loss_streak = (df['result'] == 'L').groupby(df['fighter']).cumsum()
+
+    temp_df['win_streak'] = win_streak.where(win_streak > 0, 0)
+    temp_df['loss_streak'] = loss_streak.where(loss_streak > 0, 0)
+
+    df = pd.concat([df, temp_df], axis=1)
+
+    return df
+
+
+def calculate_avg(df, column):
+    df['avg_' + column + "_per_min"] = df[column] / df['total_fight_time']
+
+    return df
+    
+def differential(df, column):
+    group = df.groupby('fight_num',group_keys=False)[column]
+    # Get difference of stats
+    differ = group.diff()
+    nan_indices = differ.isna()
+    # Back fill nan with counterpart
+    differ = differ.fillna(method='bfill')
+    # Inverse counter part with nan indices
+    differ[nan_indices] *= -1
+    
+    df[column + "_differential"] = differ.astype(float)
+    return df
+
+
+def lag(df, column, step):
+    df[column + '_lag' + str(step)] = df.groupby('fighter')[column].shift(step).fillna(0).astype(int)
+    return df
+
+
+def calculate_weighted_avg(df, column):
+    group = df.groupby('fighter')[column]
+
+    # Calculate weighted avg of previous 3 fights
+    three = 0.6 * group.shift(1) + 0.3 * group.shift(2) + 0.1 * group.shift(3)
+    # Calculate weighted avg of previous 2 fights if only 2 fights
+    two = three.fillna(0.7 * group.shift(1) + 0.3 * group.shift(2))
+    # Fill if only 1 fight
+    one = two.fillna(group.shift(1))
+    weighted_avg = one.fillna(0)
+    
+    df[column + "_weighted_avg"] = weighted_avg.astype(float)
+    return df
+
+
+def convert_to_minutes(x):
+    time_delta = pd.Timedelta(hours=x.hour, minutes=x.minute, seconds=x.second)
+    return time_delta.total_seconds() / 60
+
+
+def calculate_accuracy(attempts, landed):
+
+    return landed / attempts
+
+
+if __name__ == "__main__":
+    main()
--- ufc_scraper.py
+++ ufc_scraper.py
@@ -8,8 +8,13 @@ import re
 
 
 def main():
-    extract_fight_details()
-    # extract_fighter_stats()
+    # fights_df = pd.read_csv("fights.csv")
+    # print(fights_df['event'])
+
+    # fighters_df = pd.read_csv("fighters.csv")
+    # extract_fighter_stats(fighters_df)
+    extract_fight_details(None)
+    
 
 
 def get_events() -> List[str]:
@@ -19,19 +24,24 @@ def get_events() -> List[str]:
         List[str]: links to ufc events
     """
     event_links = []
-    # url = "http://www.ufcstats.com/statistics/events/completed?page=all"
-    url = "http://www.ufcstats.com/statistics/events/completed"
+    url = "http://www.ufcstats.com/statistics/events/completed?page=all"
+    # url = "http://www.ufcstats.com/statistics/events/completed"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     # Find all links on the page with the class 'b-link b-link_style_black'
-    links = soup.find_all('a', class_='b-link b-link_style_black')
-
+    link = soup.find('a', class_='b-link b-link_style_black')
+    
     # Extract the href attribute from each link and append it to event_links
-    for link in links:
+    # with open('cards.csv', 'r') as f:
+    while link:
+        event = link.text.strip()
+        # if event in f.read():
+        #     return event_links
         href = link['href']
         event_links.append(href)
+        link = link.find_next('a', class_='b-link b-link_style_black')
     
-    return event_links[:5]
+    return event_links[::-1]
 
 
 def extract_event_details():
@@ -42,7 +52,6 @@ def extract_event_details():
         response = requests.get(event)
         soup = BeautifulSoup(response.content, "html.parser")
         event = soup.find("span", class_="b-content__title-highlight").text.strip()
-        
         row = {"event" : event}
         
         for li in soup.find_all("li", class_="b-list__box-list-item"):
@@ -55,98 +64,199 @@ def extract_event_details():
             fight_details = fight['data-link']
             links.append(fight_details)
         
-        row['fight_details'] = links
+        row['fight_details'] = links[::-1]
         rows.append(row)
+        
     return rows
 
 
-def extract_fight_details():
+def extract_fight_details(df):
     data = extract_event_details()
     rows = []
+    fight_num = 0
     for card in data:
         date = card['date']
         location = card['location']
         event = card['event']
-        
+        print(event)
         for i, fight in enumerate(card['fight_details'], start=1):
-            row = {"fight_num" : i, "date": date, "location": location, "event" : event}
+            fight_num += 1
+            row = {"fight_num" : fight_num, "date": date, "location": location, "event" : event}
             response = requests.get(fight)
             soup = BeautifulSoup(response.content, "html.parser")
             names = [name.text.strip() for name in soup.find_all("h3", class_="b-fight-details__person-name")]
             result = soup.find("i", class_="b-fight-details__person-status").text.strip()
-            row["result"] = result
+            weight_class = soup.find("i", class_="b-fight-details__fight-title").text.strip()
+            if result == "W":
+                row["result_1"] = result
+                row["result_2"] = "L"
+            elif result == "L":
+                row["result_1"] = result
+                row["result_2"] = "W"
+            else:
+                row["result_1"] = result
+                row["result_2"] = result
+                
+                
+            row['weight_class'] = weight_class
             
             for i, name in enumerate(names, start=1):
                 row[f'fighter_{i}'] = name
             
-                # Add fight details (method, round, time, format, ref)
-                details = [i for detail in soup.find_all("p", class_="b-fight-details__text") for i in detail.stripped_strings]
-                for i in range(0, len(details), 2):
-                    if details[i] != "Details:":
-                        row[details[i].strip(":").lower()] = details[i + 1]
-                    elif row['method'] == "TKO - Doctor's Stoppage":
+            # Add fight details (method, round, time, format, ref)
+            details = [i for detail in soup.find_all("p", class_="b-fight-details__text") for i in detail.stripped_strings]
+            for i in range(0, len(details), 2):
+                if details[i] != "Details:":
+                    if details[i] == "Referee:" and details[i + 1] == "Details:":
                         row[details[i].strip(":").lower()] = None
+                        try:
+                            row[details[i+1].strip(":").lower()] = details[i+2]
+                        except IndexError:
+                            row[details[i+1].strip(":").lower()] = None
                         break
                     else:
-                        row[details[i].strip(":").lower()] = ' '.join([part.strip() for part in details[i+1:][0].split('\n')])
-                        break
-
-            stats = soup.find_all("tbody", class_="b-fight-details__table-body")
-            
-            for j in range(len(stats)):
-                data = [i for i in stats[j].stripped_strings]
-
-                if j < 2:
-                    CATEGORIES = [    
-                    ("fighter", [0, 20, 40, 60, 80]),
-                    ("kd", [2, 22, 42, 62, 82]),
-                    ("sig_str", [4, 24, 44, 64, 84]),
-                    ("sig_str_%", [6, 26, 46, 66, 86]),
-                    ("total_str", [8, 28, 48, 68, 88]),
-                    ("td", [10, 30, 50, 70, 90]),
-                    ("td_%", [12, 32, 52, 72, 92]),
-                    ("sub_attempt", [14, 34, 54, 74, 94]),
-                    ("reversal", [16, 36, 56, 76, 96]),
-                    ("ctrl", [18, 38, 58, 78, 98])
-                    ]
+                        row[details[i].strip(":").lower()] = details[i + 1]
+                elif row['method'] == "TKO - Doctor's Stoppage":
+                    row[details[i].strip(":").lower()] = None
+                    break
                 else:
-                    CATEGORIES = [    
-                    ("fighter", [0, 18, 36, 54, 72]),
-                    ("sig_str", [2, 20, 38, 56, 74]),
-                    ("sig_str%", [4, 22, 40, 58, 76]),
-                    ("head", [6, 24, 42, 60, 78]),
-                    ("body", [8, 26, 44, 62, 80]),
-                    ("leg", [10, 28, 46, 64, 82]),
-                    ("distance", [12, 30, 48, 66, 84]),
-                    ("clinch", [14, 32, 50, 68, 86]),
-                    ("ground", [16, 34, 52, 70, 88]),
-                    ]
-                inc = 0
-                current_round = None
-                for i in range(0, len(data), 2):
-                    for category_name, category_indices in CATEGORIES:
-                        if i in category_indices:
-                            key = category_name
-                            break
                     try:
-                        if data[i+inc].startswith("Round"):
-                            current_round = data[i+inc].replace(" ", "_").lower()
-                            inc += 1
-
-                    except IndexError:
-                        break
-                    if key != "fighter":
-                        if current_round is not None:
-                            row[current_round + "_" + key + "_1"] = data[i+inc]
-                            row[current_round + "_" + key + "_2"] = data[i+1+inc]
+                        if "\n" in details[i+1:][0]:
+                            row[details[i].strip(":").lower()] = ' '.join([part.strip() for part in details[i+1:][0].split('\n')])
                         else:
-                            row["total_" + key + "_1"] = data[i+inc]
-                            row["total_" + key + "_2"] = data[i+1+inc]
-            rows.append(row)  
+                            row[details[i].strip(":").lower()] = " ".join(details[i+1:])
+                    except IndexError:
+                            row[details[i].strip(":").lower()] = None
+                    break
+            try:
+                stats = [i for i in soup.find("tbody", class_="b-fight-details__table-body").stripped_strings]
+                header = [i for i in soup.find("thead", class_="b-fight-details__table-head").stripped_strings]
+                row = totals(stats, header, row)
+            except AttributeError:
+                pass
+            # breakdown of fight stats, round by round and strike breakdown
+            # stats = soup.find_all("tbody", class_="b-fight-details__table-body")
+            # all_stats(stats, row)
+            # only total
+            
+            rows.append(row)
+            
+    if df is not None:
+        temp = pd.DataFrame(rows)
+        df = pd.concat(df, temp)
+    else:
+        df = pd.DataFrame(rows)
     
-    df = pd.DataFrame(rows)
+    # df.to_csv("fights_rbr.csv", index=False)
+    df.to_csv("raw_fight_totals.csv", index=False)
+    df['event'].to_csv("cards.csv", index=False)
+
+
+def totals(stats, header, row):
+    # Pair stats with headers
+    pairs = zip(stats[::2], stats[1::2], header)
+
+    # Iterate through the pairs
+    for pair in pairs:
+        item1, item2, key = pair
+        
+        if "of" in item1 and key != 'Fighter':
+            key = key.replace(".", "").strip().replace("Total ", "").replace(" ", "_").lower()
+            landed_1, attempts_1 = item1.split("of")
+            landed_2, attempts_2 = item2.split("of")
+            landed_1, attempts_1, landed_2, attempts_2 = float(landed_1), float(attempts_1), float(landed_2), float(attempts_2)
+            avoided_1 = attempts_2 - landed_2
+            avoided_2 = attempts_1 - landed_1
+                
+            row[key + "_landed_1"] = landed_1
+            row[key + "_landed_2"] = landed_2
+            row[key + "_attempts_1"] = attempts_1
+            row[key + "_attempts_2"] = attempts_2
+            row[key + "_received_1"] = landed_2
+            row[key + "_received_2"] = landed_1
+            row[key + "_avoided_1"] = avoided_1
+            row[key + "_avoided_2"] = avoided_2
+            if key == "str":
+                per_1 = 0 if attempts_1 == 0 else landed_1 / attempts_1
+                per_2 = 0 if attempts_2 == 0 else landed_2 / attempts_2
+                
+                row[key + "_percent_1"] = per_1
+                row[key + "_percent_2"] = per_2
+        elif "%" in key:
+            key = key.strip("%").replace(".", "").strip().replace(" ", "_").lower()
+            row[key + "_percent_1"] = item1
+            row[key + "_percent_2"] = item2
+        else:
+            key = key.strip("%").replace(".", "").strip().replace(" ", "_").lower()
+            row[key + "_1"] = item1
+            row[key + "_2"] = item2
+            if key == "kd":
+                row[key + "_received_1"] = item2
+                row[key + "_received_2"] = item1
     
-    df.to_csv("fights.csv", index=False)
+    for i in range(1, 3):
+        if row[f"str_attempts_{i}"] == 0:
+            row[f"sig_reg_mixture_{i}"] = 0
+            row[f"sig_reg_percent_{i}"] = 0
+        else:
+            row[f"sig_reg_mixture_{i}"] = row[f"sig_str_attempts_{i}"] / row[f"str_attempts_{i}"]
+            row[f"sig_reg_percent_{i}"] = (row[f"str_landed_{i}"] + row[f"sig_str_landed_{i}"]) / (row[f"str_attempts_{i}"] + row[f"sig_str_attempts_{i}"])
+            
+    return row
+                
+              
+            
+def all_stats(stats, row):
+    for j in range(len(stats)):
+        data = [i for i in stats[j].stripped_strings]
+
+        if j < 2:
+            CATEGORIES = [    
+            ("fighter", [0, 20, 40, 60, 80]),
+            ("kd", [2, 22, 42, 62, 82]),
+            ("sig_str", [4, 24, 44, 64, 84]),
+            ("sig_str_%", [6, 26, 46, 66, 86]),
+            ("str", [8, 28, 48, 68, 88]),
+            ("td", [10, 30, 50, 70, 90]),
+            ("td_%", [12, 32, 52, 72, 92]),
+            ("sub_attempt", [14, 34, 54, 74, 94]),
+            ("reversal", [16, 36, 56, 76, 96]),
+            ("ctrl", [18, 38, 58, 78, 98])
+            ]
+        else:
+            CATEGORIES = [    
+            ("fighter", [0, 18, 36, 54, 72]),
+            ("sig_str", [2, 20, 38, 56, 74]),
+            ("sig_str%", [4, 22, 40, 58, 76]),
+            ("head", [6, 24, 42, 60, 78]),
+            ("body", [8, 26, 44, 62, 80]),
+            ("leg", [10, 28, 46, 64, 82]),
+            ("distance", [12, 30, 48, 66, 84]),
+            ("clinch", [14, 32, 50, 68, 86]),
+            ("ground", [16, 34, 52, 70, 88]),
+            ]
+        inc = 0
+        current_round = None
+        for i in range(0, len(data), 2):
+            for category_name, category_indices in CATEGORIES:
+                if i in category_indices:
+                    key = category_name
+                    break
+            try:
+                if data[i+inc].startswith("Round"):
+                    current_round = data[i+inc].replace(" ", "_").lower()
+                    inc += 1
+
+            except IndexError:
+                break
+            if key != "fighter":
+                if current_round is not None:
+                    row[current_round + "_" + key + "_1"] = data[i+inc]
+                    row[current_round + "_" + key + "_2"] = data[i+1+inc]
+                else:
+                    row["total_" + key + "_1"] = data[i+inc]
+                    row["total_" + key + "_2"] = data[i+1+inc]
+    return row
 
 
 def get_fighter_links() -> List[str]:
@@ -158,8 +268,8 @@ def get_fighter_links() -> List[str]:
     alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
     fighter_links = set()
     for letter in alphabet:
-        url = f"http://www.ufcstats.com/statistics/fighters?char={letter}&page=all"
-        # url = "http://www.ufcstats.com/statistics/fighters"
+        # url = f"http://www.ufcstats.com/statistics/fighters?char={letter}&page=all"
+        url = "http://www.ufcstats.com/statistics/fighters"
         session = requests.Session()
         response = session.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
@@ -184,6 +294,8 @@ def extract_fighter_stats():
         record = soup.find("span", class_="b-content__title-record").text.strip()
 
         row = {"fighter" : name, "mma_record" : record}
+        foo = soup.find("tr", class_="b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click")
+
         # extract the attributes from the list items
         for stat in soup.find_all("li", class_="b-list__box-list-item b-list__box-list-item_type_block"):
             values = [item.strip() for item in stat.text.split(":")]
