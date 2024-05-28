23371998a51fc4dc1507e4ed6fc48c19ed79efc8
unknown <Jacobrdilonardo@gmail.com>
2024-05-28 13:26:36 +1000

    integrate future fight functions to scraper class

--- scripts/ufc_scraper.py
+++ scripts/ufc_scraper.py
@@ -364,6 +364,44 @@ class UfcScraper:
         df = df.sort_values('fighter')
         df.to_csv("data/fighter_stats.csv", index=False)
 
+    def get_next_event_link(self) -> List[str]:
+        url = "http://www.ufcstats.com/statistics/events/upcoming"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        # Find all links on the page with the soup BeautifulSoup('a', class_ 'b-link b-link_style_black'
+        link = soup.find_all('a', class_='b-link b-link_style_black')[0]
+        href = link['href']
+        
+        return href
+
+    def get_future_matchups(self) -> Tuple[str, str, str]:
+        event_link = self.get_next_event_link()
+        response = requests.get(event_link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        fighters = [fighter.text.strip() for fighter in soup.find_all('a', class_="b-link b-link_style_black")]
+        list_items = soup.find_all('li', class_='b-list__box-list-item')
+        weightclass_column = []
+        matchups = []
+        
+        for row in soup.find_all('tr'):
+            cells = row.find_all('td')
+            if len(cells) > 0 and cells[6].text != 'Weight class':
+                if row.find('img'):
+                    weightclass_column.append(cells[6].text.strip() + " Title")
+                else:
+                    weightclass_column.append(cells[6].text.strip())
+
+        for item in list_items:
+            if 'Date:' in item.text:
+                date = item.text.replace('Date:', '').strip()
+                break
+            
+        for i in range(0, len(fighters), 3):
+            matchup = (fighters[i], fighters[i+1])
+            matchups.append(matchup)
+            
+        return (date, matchups, weightclass_column)
+
 
 def main():
     try:
